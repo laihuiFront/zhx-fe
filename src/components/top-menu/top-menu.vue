37d3872bd76140af1c0a912f4bf67a25909cd6d2
unknown <laihui@cdcloud.cmiotit.com>
2019-01-22 20:41:10 +0800

    菜单

--- src/components/top-menu/top-menu.vue
+++ src/components/top-menu/top-menu.vue
@@ -1,15 +1,74 @@
 <template>
-  <section id="top-menu">顶级菜单</section>
+  <section id="top-menu">
+    <img src="./group.png" alt="" class="logo">
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3" disabled>消息中心</el-menu-item>
+      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    </el-menu>
+    <el-dropdown class="">
+      <span class="el-dropdown-link">
+        {{下拉菜单}}<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item command="a">黄金糕</el-dropdown-item>
+        <el-dropdown-item command="b">狮子头</el-dropdown-item>
+        <el-dropdown-item command="c">螺蛳粉</el-dropdown-item>
+        <el-dropdown-item command="d" disabled>双皮奶</el-dropdown-item>
+        <el-dropdown-item command="e" divided>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </section>
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
 export default {
-  name: 'topMenu'
+  name: 'topMenu',
+  computed: {
+    ...mapGetters([
+      'userInfo'
+      // ...
+    ])
+  }
 }
 </script>
 
 <style lang="scss">
 #top-menu{
-  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  height: 80px;
+  background: #132c51;
+  .logo{
+    width: 295px;
+    height: 34px;
+  }
+  .el-dropdown{
+    .el-dropdown-link{
+      color:#fff;
+      font-size: 14px;
+    }
+  }
 }
 </style>
