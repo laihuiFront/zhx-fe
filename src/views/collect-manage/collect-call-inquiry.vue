4304f7c3236cd5a1c5931cc96979063e6a882f9c
zy <>
2019-01-30 17:45:38 +0800

    collect-manage

--- src/views/collect-manage/collect-call-inquiry.vue
+++ src/views/collect-manage/collect-call-inquiry.vue
@@ -1,10 +1,152 @@
 <template>
-  <div id="collect-call-inquiry">来电查询</div>
+  <div id="collect-call-inquiry">
+
+    <el-form :inline="true" ref="form1" :model="form1" label-width="80px">
+      <el-form-item prop="val1">
+        <el-radio-group v-model="form1.val1">
+          <el-radio label="精确"></el-radio>
+          <el-radio label="模糊"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item prop="val2">
+        <el-autocomplete
+          class="inline-input"
+          v-model="form1.val2"
+          :fetch-suggestions="querySearch"
+          placeholder="委托方"
+        ></el-autocomplete>
+      </el-form-item>
+      <el-form-item prop="val3">
+        <el-input
+          v-model="form1.val3"
+          placeholder="姓名"
+          clearable
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="val4">
+        <el-input
+          v-model="form1.val4"
+          placeholder="电话"
+          clearable
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="val5">
+        <el-input
+          v-model="form1.val5"
+          placeholder="地址"
+          clearable
+        ></el-input>
+      </el-form-item><el-form-item prop="val6">
+        <el-input
+          v-model="form1.val6"
+          placeholder="证件号"
+          clearable
+        ></el-input>
+      </el-form-item><el-form-item prop="val7">
+        <el-input
+          v-model="form1.val7"
+          placeholder="卡号"
+          clearable
+        ></el-input>
+      </el-form-item><el-form-item prop="val8">
+        <el-input
+          v-model="form1.val8"
+          placeholder="单位名称"
+          clearable
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="text" icon="el-icon-search">查询</el-button>
+        <el-button
+          type="text"
+          icon="el-icon-refresh"
+          @click="resetForm('form1')"
+        >重置</el-button
+        >
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="tableData"
+      tooltip-effect="dark"
+      style="width: 100%"
+    >
+      <el-table-column
+        v-for="(item, index) in tablecol_data"
+        v-bind="item"
+        :key="index"
+      ></el-table-column>
+      <el-table-column label="操作">
+        <el-button>
+          评语
+        </el-button>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'collectCallInquiry'
+  name: 'collectCallInquiry',
+  data(){
+    return {
+      form1:{
+        val1:'精确',
+        val2:'',
+        val3:'',
+        val4:'',
+        val5:'',
+        val6:'',
+        val7:'',
+        val8:'',
+      },
+      tableData: [],
+      tablecol_data:[
+        {
+          prop:'a12',
+          label:'个案序列号'
+        },
+        {
+          prop:'a12',
+          label:'姓名'
+        },{
+          prop:'a12',
+          label:'卡号'
+        },{
+          prop:'a12',
+          label:'委案金额'
+        },{
+          prop:'a12',
+          label:'委案日期'
+        },{
+          prop:'a12',
+          label:'催收状态'
+        },{
+          prop:'a12',
+          label:'上次通电'
+        },{
+          prop:'a12',
+          label:'承诺还款金额'
+        },{
+          prop:'a12',
+          label:'已还款金额'
+        },{
+          prop:'a12',
+          label:'账龄'
+        },{
+          prop:'a12',
+          label:'催收员'
+        },
+      ]
+    }
+  },
+  methods:{
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    querySearch(queryString, cb) {
+      cb([]);
+    },
+  }
 }
 </script>
 
--- src/views/collect-manage/collect-status-statistics.vue
+++ src/views/collect-manage/collect-status-statistics.vue
@@ -1,16 +1,170 @@
 <template>
-  <div id="collect-status-statistics">催收状况统计</div>
+  <div id="collect-status-statistics">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="当日电催跟进量" name="first">
+        <el-form :inline="true" ref="form1" :model="form1" label-width="80px">
+          <el-form-item prop="val1">
+            <el-date-picker
+              v-model="form1.val1"
+              type="daterange"
+              align="right"
+              unlink-panels
+              range-separator="至"
+              start-placeholder="催收开始日期"
+              end-placeholder="催收结束日期"
+            >
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="text" icon="el-icon-search">查询</el-button>
+            <el-button
+              type="text"
+              icon="el-icon-refresh"
+              @click="resetForm('form1')"
+            >重置</el-button
+            >
+          </el-form-item>
+        </el-form>
+        <el-table :data="tableData1" style="width: 100%">
+          <el-table-column
+            v-for="(item, index) in tablecol_data1"
+            :key="index"
+            v-bind="item"
+          ></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="催收状态统计" name="second">
+        <el-form :inline="true" ref="form2" :model="form2" label-width="80px">
+          <el-form-item prop="val1">
+            <el-autocomplete
+              class="inline-input"
+              v-model="form2.val1"
+              :fetch-suggestions="querySearch"
+              placeholder="委托方"
+            ></el-autocomplete>
+          </el-form-item>
+          <el-form-item prop="val2">
+            <el-autocomplete
+              class="inline-input"
+              v-model="form2.val2"
+              :fetch-suggestions="querySearch"
+              placeholder="批次号"
+            ></el-autocomplete>
+          </el-form-item>
+          <el-form-item prop="val3">
+            <el-autocomplete
+              class="inline-input"
+              v-model="form2.val3"
+              :fetch-suggestions="querySearch"
+              placeholder="逾期账龄"
+            ></el-autocomplete>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="text" icon="el-icon-search">查询</el-button>
+            <el-button
+              type="text"
+              icon="el-icon-refresh"
+              @click="resetForm('form2')"
+            >重置</el-button
+            >
+          </el-form-item>
+        </el-form>
+        <el-table :data="tableData2" style="width: 100%">
+          <el-table-column
+            v-for="(item, index) in tablecol_data2"
+            :key="index"
+            v-bind="item"
+          ></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="批次分类统计" name="third">
+        <el-form :inline="true" ref="form3" :model="form3" label-width="80px">
+          <el-form-item prop="val1">
+            <el-autocomplete
+              class="inline-input"
+              v-model="form3.val1"
+              :fetch-suggestions="querySearch"
+              placeholder="委托方"
+            ></el-autocomplete>
+          </el-form-item>
+          <el-form-item prop="val2">
+            <el-autocomplete
+              class="inline-input"
+              v-model="form3.val2"
+              :fetch-suggestions="querySearch"
+              placeholder="批次号"
+            ></el-autocomplete>
+          </el-form-item>
+          <el-form-item prop="val3">
+            <el-autocomplete
+              class="inline-input"
+              v-model="form3.val3"
+              :fetch-suggestions="querySearch"
+              placeholder="逾期账龄"
+            ></el-autocomplete>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="text" icon="el-icon-search">查询</el-button>
+            <el-button
+              type="text"
+              icon="el-icon-refresh"
+              @click="resetForm('form3')"
+            >重置</el-button
+            >
+          </el-form-item>
+        </el-form>
+        <el-table :data="tableData2" style="width: 100%">
+          <el-table-column
+            v-for="(item, index) in tablecol_data2"
+            :key="index"
+            v-bind="item"
+          ></el-table-column>
+        </el-table>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'collectStatusStatistics'
-}
+  name: "collectStatusStatistics",
+  data() {
+    return {
+      activeName: "first",
+      form1: { val1: null },
+      tableData1: [],
+      tablecol_data1: [
+        { prop: "a12", label: "时段" },
+        { prop: "a12", label: "有效通电量" },
+        { prop: "a12", label: "总通电量" },
+        { prop: "a12", label: "通电案量" }
+      ],
+      form2:{val1:null,val2:null,val3:null},
+      tableData2: [],
+      tablecol_data2:[
+      ],
+      val1_data: [],//tab2 委托方
+      val3_data: [],//tab2 逾期账龄
+      form3:{val1:null,val2:null,val3:null},
+      tableData3: [],
+      tablecol_data3:[]
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    querySearch(queryString, cb){
+      cb([]);
+    },
+  }
+};
 </script>
 
 <style lang="scss">
-#collect-status-statistics{
-  
+#collect-status-statistics {
 }
 </style>
-
