1d7d966494052686d8405bd9e705a2bdbb555a5d
zy <>
2019-02-20 16:50:59 +0800

    我的案件 添加多选

--- src/views/collect-manage/collect-my-case.vue
+++ src/views/collect-manage/collect-my-case.vue
@@ -53,12 +53,20 @@
                   </el-select>
                 </el-form-item>
                 <el-form-item prop="val1">
-                  <el-autocomplete
+                  <el-select
                     v-model="form.val1"
-                    :fetch-suggestions="querySearch"
-                    :trigger-on-focus="false"
-                    placeholder="请输入批次号">
-                  </el-autocomplete>
+                    multiple
+                    filterable
+                    remote
+                    placeholder="请输入批次号"
+                    :remote-method="querySearch">
+                    <el-option
+                      v-for="item in val1_data"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value">
+                    </el-option>
+                  </el-select>
                 </el-form-item>
                 <el-form-item prop="val3">
                   <el-input
@@ -547,7 +555,7 @@ export default {
         val30: "" //跟进次数下限
       },
       val0_data: [],  //委托方
-      val1_data: [],  //
+      val1_data: [],  //批次号
       val4_data: [],  //地区
       val8_data: [],  //逾期账龄
       // 未退案0/正常1/暂停2/关档3/退档4/全部5
@@ -800,14 +808,14 @@ export default {
       this.getMainData();
     },
     //查询批次号
-    querySearch(queryString,cb){
+    querySearch(queryString){
       batchNo({batchNo:queryString}).then((data)=>{
-        cb(data.reduce((acc,item)=>{
+        this.val1_data = data.reduce((acc,item)=>{
           acc.push({
             value:item.batchNo
           })
           return acc;
-        },[]));
+        },[]);
       });
     },
     handleCurrentChange(currentPage){
--- src/views/collect-manage/collect-repayment-statistics.vue
+++ src/views/collect-manage/collect-repayment-statistics.vue
@@ -72,12 +72,20 @@
           </el-select>
         </el-form-item>
         <el-form-item prop="val3">
-          <el-autocomplete
-            class="inline-input"
+          <el-select
             v-model="form1.val3"
-            :fetch-suggestions="querySearch"
-            placeholder="批次号"
-          ></el-autocomplete>
+            multiple
+            filterable
+            remote
+            placeholder="请输入批次号"
+            :remote-method="querySearch">
+            <el-option
+              v-for="item in val3_data"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
         </el-form-item>
         <el-form-item prop="val4">
           <el-select
@@ -343,6 +351,7 @@ export default {
       val2_data: [],
       val4_data: [],
       val8_data: [],  //部门
+      val3_data: [],  //批次号
     };
   },
   created() {
@@ -380,16 +389,15 @@ export default {
       this.tablecol_data = this[`tablecol_raw${v * 1 + 1}`];
       this.getMainData();
     },
-    querySearch(queryString, cb) {
-      batchNo({ batchNo: queryString }).then(data => {
-        cb(
-          data.reduce((acc, item) => {
-            acc.push({
-              value: item.batchNo
-            });
-            return acc;
-          }, [])
-        );
+    //查询批次号
+    querySearch(queryString){
+      batchNo({batchNo:queryString}).then((data)=>{
+        this.val3_data = data.reduce((acc,item)=>{
+          acc.push({
+            value:item.batchNo
+          })
+          return acc;
+        },[]);
       });
     },
     resetForm(formName) {
