fe278c6aea39be3d4cb061902f2c31cba7e45a83
laihuiFront <15549405490@163.com>
2019-02-21 23:00:04 +0800

    新增还款记录

--- src/views/synergistic-manage/synergistic-repayment-record.vue
+++ src/views/synergistic-manage/synergistic-repayment-record.vue
@@ -55,6 +55,74 @@
       :total="total"
       class="pagination-wrap"
     ></el-pagination>
+    <el-dialog
+      :title="dialogData.title"
+      class="dialog-wrap"
+      :visible.sync="dialogData.editVisible"
+      :close-on-click-modal="false"
+      width="50%"
+    >
+      <el-form
+        :model="recordInfo"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="120px"
+        class="add-form"
+      >
+        <el-form-item label="对应个案" prop="dyga">
+          <el-select v-model="recordInfo.dyga" clearable placeholder="请选择对应个案">
+            <!-- <el-option
+              v-for="item in positionList"
+              :key="item.job"
+              :label="item.position"
+              :value="item.position"
+            ></el-option> -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="回收催收员" prop="dyga">
+          <el-select v-model="recordInfo.dyga" clearable placeholder="请选择回收催收员">
+            <!-- <el-option
+              v-for="item in positionList"
+              :key="item.job"
+              :label="item.position"
+              :value="item.position"
+            ></el-option> -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="还款日期" prop="dyga">
+          <el-date-picker
+            clearable
+            v-model="recordInfo.joinTime"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="请选择还款日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item label="确认还款金额" prop="dyga">
+          <el-input v-model="recordInfo.mobile" clearable placeholder="确认还款金额"></el-input>
+        </el-form-item>
+        <el-form-item label="还款人" prop="dyga">
+           <el-input v-model="recordInfo.mobile" clearable placeholder="请输入还款人"></el-input>
+        </el-form-item>
+        <el-form-item label="还款方式" prop="dyga">
+          <el-select v-model="recordInfo.dyga" clearable placeholder="请选择还款方式">
+            <!-- <el-option
+              v-for="item in positionList"
+              :key="item.job"
+              :label="item.position"
+              :value="item.position"
+            ></el-option> -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="备注" prop="dyga" class="whole">
+          <el-input type="textarea" :rows="4" v-model="recordInfo.remark"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="footer">
+        <el-button @click="onClickCancel">取 消</el-button>
+        <el-button type="primary" @click="onClickSave">保 存</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -70,6 +138,14 @@ export default {
       tabName: '1',
       recordList: [],
       total:0,
+      dialogData:{
+        title:'',
+        editVisible: true
+      },
+      rules: {
+
+      },
+      recordInfo:{},
       queryForm:{
         pageNum: 1,
         pageSize: 10,
@@ -78,7 +154,9 @@ export default {
   },
   methods: {
     onSelectRow(){},
-    onClickQuery(){}
+    onClickQuery(){},
+    onClickCancel(){},
+    onClickSave(){}
   }
 }
 </script>
@@ -92,6 +170,24 @@ export default {
     margin-right: 12px;
     display: inline-block;
     color: #66b1ff;
+  }
+  .add-form {
+    display: flex;
+    flex-wrap: wrap;
+    .el-form-item {
+      display: flex;
+      width: 50%;
+      &.whole{
+        width: 100%;
+      }
+      .el-form-item__content {
+        flex: 1;
+        margin-left: 0 !important;
+        .el-select {
+          width: 100%;
+        }
+      }
+    }
   }
 }
 </style>
