e1836f71cd5313f8fca5cd8a216bc22aa596661f
laihui <laihui@ctu.chinamobile.com>
2019-03-04 16:46:56 +0800

    月统计样式修改

--- src/views/statistics-manage/statistics-month.vue
+++ src/views/statistics-manage/statistics-month.vue
@@ -1,123 +1,104 @@
 <template>
-  <div id="statistics-month">
-<el-row :gutter="20">
-  <el-col :span="22">
-  	<div class="grid-content bg-purple">
-  		<el-form :inline="true" :model="formInline" class="demo-form-inline">
-  <el-form-item >
-    <el-select v-model="formInline.odv" multiple collapse-tags  filterable  placeholder="请选择催收员" clearable>
-    <el-option
-      v-for="item in PersonList"
-      :key="item.id"
-      :label="item.userName"
-      :value="item.id">
-    </el-option>
-  </el-select> 
-  </el-form-item>
-  <el-form-item>
-    <el-select v-model="formInline.area" placeholder="请选择催收区域" clearable>
-    <el-option
-      v-for="item in areaList"
-      :key="item.id"
-      :label="item.name"
-      :value="item.id">
-    </el-option>
-  </el-select>   
-  </el-form-item>
-  <el-form-item >
-  <el-select v-model="formInline.client" placeholder="请选择委托方" clearable>
-    <el-option
-      v-for="item in clientList"
-      :key="item.value"
-      :label="item.name"
-      :value="item.id">
-    </el-option>
-  </el-select>
-  </el-form-item>
-  <el-form-item >
-   <div class="block">
-    <el-date-picker
-    	
-      v-model="formInline.time"
-      type="month"
-      placeholder="选择开始月份"
+  <div id="statistics-month" class="page-wraper-sub">
+    <el-form ref="form" :model="formInline" :inline="true" class="query-wrap">
+      <el-form-item>
+        <el-select v-model="formInline.odv" multiple collapse-tags  filterable  placeholder="请选择催收员" clearable>
+          <el-option
+            v-for="item in PersonList"
+            :key="item.id"
+            :label="item.userName"
+            :value="item.id">
+          </el-option>
+        </el-select> 
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="formInline.area" placeholder="请选择催收区域" clearable>
+          <el-option
+            v-for="item in areaList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id">
+          </el-option>
+        </el-select>  
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="formInline.client" placeholder="请选择委托方" clearable>
+          <el-option
+            v-for="item in clientList"
+            :key="item.value"
+            :label="item.name"
+            :value="item.id">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker
+          v-model="formInline.time"
+          type="month"
+          placeholder="选择开始月份"
+          >
+        </el-date-picker>至
+        <el-date-picker
+          v-model="formInline.time2"
+          type="month"
+          placeholder="选择结束月份"
+          >
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="text" icon="el-icon-search" @click="query()">开始统计</el-button>
+        <el-button type="text" icon="el-icon-refresh" @click=clench>重置</el-button> 
+      </el-form-item>
+      <el-form-item class="operation-item">
+        <el-button type="primary" @click="onSubmit">导出查询结果</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="tableData3"
+      border
+      stripe
+      show-summary
+      height="350"
+      :summary-method="getSummaries"
+      style="width: 100%"
+      class="table-wrap">
+      <el-table-column
+        prop="odv"
+        align="center"
+        label="催收员"
       >
-    </el-date-picker>至
-    <el-date-picker
-    	
-      v-model="formInline.time2"
-      type="month"
-      placeholder="选择结束月份"
-      >
-    </el-date-picker>
-  </div>
-  
-  </el-form-item>
-  <el-form-item>
-  <el-button type="text" icon="el-icon-search" @click="query()">开始统计</el-button>
-  </el-form-item>
-  <el-form-item>
-  <el-button type="text" icon="el-icon-refresh" @click=clench>重置</el-button> 
-  </el-form-item>
-
-</el-form>
-  	</div></el-col>
-  <el-col :span="2">
-  	<div class="grid-content bg-purple">
- <el-form :inline="true"  class="demo-form-inline">
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit">导出查询结果</el-button>
-  </el-form-item>
-</el-form>
-  	</div></el-col>
-</el-row>
-  
-  <el-table
-    :data="tableData3"
-    border
-    stripe
-     show-summary
-     height="350"
-     :summary-method="getSummaries"
-    style="width: 100%">
-    <el-table-column
-      prop="odv"
-      align="center"
-      label="催收员"
-     >
-    </el-table-column>
-    <el-table-column :label="item.area" align="center" v-for="(item,index) in dataList" :key="item.area">
-      <el-table-column
-        label="有效通电"
-        align="center"
-        prop="countConPhoneNum"
-       >
-        <template slot-scope="scope">
-          {{ scope.row.list[index].countConPhoneNum }}
-        </template>
       </el-table-column>
-      <el-table-column
-        label="总通电量"
-        align="center"
-        prop="countPhoneNum"
-       >
-        <template slot-scope="scope">
-          {{ scope.row.list[index].countPhoneNum }}
-        </template>
+      <el-table-column :label="item.area" align="center" v-for="(item,index) in dataList" :key="item.area">
+        <el-table-column
+          label="有效通电"
+          align="center"
+          prop="countConPhoneNum"
+        >
+          <template slot-scope="scope">
+            {{ scope.row.list[index].countConPhoneNum }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="总通电量"
+          align="center"
+          prop="countPhoneNum"
+        >
+          <template slot-scope="scope">
+            {{ scope.row.list[index].countPhoneNum }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="个按量"
+          align="center"
+          prop="countCasePhoneNum"
+        >
+          <template slot-scope="scope">
+            {{ scope.row.list[index].countCasePhoneNum }}
+          </template>
+        </el-table-column>
       </el-table-column>
-      <el-table-column
-        label="个按量"
-        align="center"
-        prop="countCasePhoneNum"
-       >
-        <template slot-scope="scope">
-          {{ scope.row.list[index].countCasePhoneNum }}
-        </template>
-      </el-table-column>
-    </el-table-column>
-  </el-table>
-    <div class="block">
-  	 <el-pagination
+    </el-table>
+    <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="currentPage4"
@@ -126,7 +107,6 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total">
     </el-pagination>
-  </div>
   </div>
 </template>
 
