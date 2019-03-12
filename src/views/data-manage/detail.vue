79b0a11247c0d420a7616bc72cc8559c28afa92c
齐飞 <qifei@ovfintech.com>
2019-03-12 16:59:37 +0800

    update

--- src/views/data-manage/detail.vue
+++ src/views/data-manage/detail.vue
@@ -3205,8 +3205,8 @@ AddtableList(this.id,this.messageForm).then((response)=>{
     display: flex;
     .left-panel{
       padding: 12px;
-      flex: 0 0 50%;
-      width: 50%;
+      flex: 0 0 35%;
+      width: 35%;
       margin-right: 24px;
       border: 1px solid #d1d1d1;
       box-sizing: border-box;
@@ -3238,8 +3238,8 @@ AddtableList(this.id,this.messageForm).then((response)=>{
       }
     }
     .right-panel{
-      flex: 0 0 50%;
-      width: 50%;
+      flex: 0 0 65%;
+      width: 65%;
     }
   }
 }
